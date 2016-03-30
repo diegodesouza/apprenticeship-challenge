82004a36557e3dbe27a24ff2fcc629a8d0979bd2
diegodesouza <diegotrus7@gmail.com>
2016-03-29 21:04:38 -0400

    Add Item

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
-import Note from './Note.jsx';
+import React from 'react'
+import Note from './Note.jsx'
+import uuid from 'node-uuid'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: uuid.v4(),
+          text: "Learn React"
+        },
+        {
+          id: uuid.v4(),
+          text: "Learn Javascript"
+        }
+      ]
+    } 
+  }
   render() {
-    return <Note />;
+    const items = this.state.items
+
+    return (
+      <div>
+        <button onClick={this.addItem}>+</button>
+        <ul>
+          {items.map(item => 
+           <li key={item.id}>{item.text}</li>
+          )} </ul>
+      </div>
+    )
+  }
+
+  addItem = () => {
+    this.setState({
+      items: this.state.items.concat(
+        [
+          {
+            id: uuid.v4(),
+            text: "New Shopping Item"
+          }
+        ]
+      )
+    })
   }
 }
