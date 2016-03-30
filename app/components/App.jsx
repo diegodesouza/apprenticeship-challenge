5f68909fcb483c28d53873f99da588707bd74eb0
diegodesouza <diegotrus7@gmail.com>
2016-03-29 23:27:18 -0400

    Implemented storage with Alt.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,49 +1,29 @@
 import React from 'react'
-import Items from './Items.jsx'
-import uuid from 'node-uuid'
+import Items from './Items'
+import ItemActions from '../actions/ItemActions'
+import ItemStore from '../stores/ItemStore'
+import AltContainer from 'alt-container'
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      items: [
-        {
-          id: uuid.v4(),
-          text: "Learn React"
-        },
-        {
-          id: uuid.v4(),
-          text: "Learn Javascript"
-        }
-      ]
-    } 
-  }
   render() {
-    const items = this.state.items
-
     return (
       <div>
         <button onClick={this.addItem}>+</button>
-        <Items 
-          items={items}
-          onEdit={this.editItem}
-          onDelete={this.deleteItem}
-        />
+        <AltContainer
+          stores={[ItemStore]}
+          inject={{items: () => ItemStore.getState().items}}
+        >
+          <Items 
+            onEdit={this.editItem}
+            onDelete={this.deleteItem}
+          />
+        </AltContainer>
       </div>
     )
   }
 
   addItem = () => {
-    this.setState({
-      items: this.state.items.concat(
-        [
-          {
-            id: uuid.v4(),
-            text: "New Shopping Item"
-          }
-        ]
-      )
-    })
+    ItemActions.create({text: "New Shopping Item"})
   }
 
   editItem = (id, text) => {
@@ -51,22 +31,12 @@ export default class App extends React.Component {
       return;
     }    
 
-    const items = this.state.items.map(item => {
-      if(item.id === id && text) {
-        item.text = text
-      }
-
-      return item
-    })
-
-    this.setState({items})
+    ItemActions.update({id, text})
   }
 
   deleteItem = (id, e) => {
     e.stopPropagation();
 
-    this.setState({
-      items: this.state.items.filter(item => item.id !== id)
-    })
+    ItemActions.delete(id)
   }
 }
